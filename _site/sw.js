bfc1cb5d2300d5711ff19fe1710430d1e8984cfa
Youi Hyon Kim <youihyonkim@Youiui-MacBookPro.local>
2024-01-04 17:28:30 +0900

    [feat] Create git Blog

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704343816';
+const cacheName = 'chirpy-1704356831';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
