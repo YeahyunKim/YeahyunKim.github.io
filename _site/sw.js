4396983e0c72913b419da6493c62eb4d3ba34400
Youi Hyon Kim <youihyonkim@Youiui-MacBookPro.local>
2024-01-04 18:05:34 +0900

    [fix] js 파일 경로 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704357472';
+const cacheName = 'chirpy-1704358936';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
