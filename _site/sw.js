536d126b8c0ca56ac08b64706fb2db4de9bfadca
Youi Hyon Kim <youihyonkim@Youiui-MacBookPro.local>
2024-01-08 16:53:46 +0900

    [Programmers] 코딩테스트 연습 > 코딩 기초 트레이닝 > 문자열 출력하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704623454';
+const cacheName = 'chirpy-1704700391';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
