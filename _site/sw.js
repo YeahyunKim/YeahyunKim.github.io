b535b13ff4d708938f085301d2022a31ef8cbfb3
Yeahyun Kim <insidesy4@gmail.com>
2024-01-09 17:59:45 +0900

    [Post] 프로그래머스 Swift 코테준비 (5)

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704718142';
+const cacheName = 'chirpy-1704790753';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
