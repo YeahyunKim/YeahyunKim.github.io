c655948da22ba6993088896c3ff798e7cc388f8a
Yeahyun Kim <insidesy4@gmail.com>
2024-01-13 22:37:14 +0900

    [Fix] 구글 드라이브 이미지 403 에러 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704980154';
+const cacheName = 'chirpy-1705153008';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
