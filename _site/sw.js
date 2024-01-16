11380db207400ec7a526a2202dc1d9aef9200c6b
Yeahyun Kim <insidesy4@gmail.com>
2024-01-16 21:15:15 +0900

    [Feat] command-text light mode style 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705406462';
+const cacheName = 'chirpy-1705407195';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
