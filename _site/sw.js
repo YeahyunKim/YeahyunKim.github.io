c1cf055a2dc4be0ea3af7dc442cc4f608cb4e4fa
Yeahyun Kim <insidesy4@gmail.com>
2024-01-16 17:31:14 +0900

    [Fix] md 테이블 표 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705153008';
+const cacheName = 'chirpy-1705393862';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
