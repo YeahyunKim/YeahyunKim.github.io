c8424b9ab6b30101bd5dbcd965e3c44eccf3ebf5
Yeahyun Kim <insidesy4@gmail.com>
2024-01-17 22:24:57 +0900

    [Fix] md h5 -> h4 로 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705497593';
+const cacheName = 'chirpy-1705497876';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
