9182f8c8b5542559bd563e281c3a1ecf71927333
Yeahyun Kim <insidesy4@gmail.com>
2024-01-22 18:52:58 +0900

    [Post] 프로그래머스 Swift 코테준비 (18) 홀짝에 따라 다른 값 반환하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1705916464';
+const cacheName = 'chirpy-1705917152';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
