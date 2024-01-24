739a713f7b78ce688d701bead5b9b75fe88e80a2
Yeahyun Kim <insidesy4@gmail.com>
2024-01-24 16:29:03 +0900

    [Post] 프로그래머스 Swift 코테준비 (21) 코드 처리하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706018504';
+const cacheName = 'chirpy-1706081308';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
