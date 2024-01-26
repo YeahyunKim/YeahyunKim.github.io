746564ace62e5b3e3478e613b7e176324101d012
Yeahyun Kim <insidesy4@gmail.com>
2024-01-26 22:02:16 +0900

    [Post] 프로그래머스 Swift 코테준비 (5) 특수문자 출력하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706273954';
+const cacheName = 'chirpy-1706274094';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
