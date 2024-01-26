ffe7860cc66b920a867c6f02f28aa5db035ddd3c
Yeahyun Kim <insidesy4@gmail.com>
2024-01-26 21:57:23 +0900

    [Post] 프로그래머스 Swift 코테준비 (3) 문자열 반복해서 출력하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706273697';
+const cacheName = 'chirpy-1706273817';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
