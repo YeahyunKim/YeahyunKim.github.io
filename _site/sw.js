edbd946c7d40dcbfb9e8fed056bdbf0fbff6d229
Yeahyun Kim <insidesy4@gmail.com>
2024-01-26 21:53:40 +0900

    [Post] 프로그래머스 Swift 코테준비 (1) 문자열 출력하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706189345';
+const cacheName = 'chirpy-1706273584';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
