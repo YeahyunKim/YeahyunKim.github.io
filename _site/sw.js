09df42cc21d3a743fde5989b2e05f68ed64b6a26
Yeahyun Kim <insidesy4@gmail.com>
2024-01-26 21:55:22 +0900

    [Post] 프로그래머스 Swift 코테준비 (2) a 와 b 출력하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706273584';
+const cacheName = 'chirpy-1706273697';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
