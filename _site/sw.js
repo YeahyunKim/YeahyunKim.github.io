eae28c0a3c8b25f8f7f4d3b567e4ba66a6466e2e
Yeahyun Kim <insidesy4@gmail.com>
2024-01-26 21:59:42 +0900

    [Post] 프로그래머스 Swift 코테준비 (4) 대소문자 바꿔서 출력하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706273817';
+const cacheName = 'chirpy-1706273954';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
