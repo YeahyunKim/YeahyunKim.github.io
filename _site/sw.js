24787e86f47cfee8a855c8e01e43cc78458fe411
Yeahyun Kim <insidesy4@gmail.com>
2024-01-27 20:36:59 +0900

    [Post] 프로그래머스 Swift 코테준비 (8) 문자열 돌리기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706355323';
+const cacheName = 'chirpy-1706355402';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
