8ffedc38d588e496bf38acaae62a68531c30f6f6
Yeahyun Kim <insidesy4@gmail.com>
2024-01-28 22:21:50 +0900

    [Post] 프로그래머스 Swift 코테준비 (11) 문자열 섞기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706355548';
+const cacheName = 'chirpy-1706448092';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
