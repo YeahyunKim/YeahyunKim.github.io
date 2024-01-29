38fac8f962f0424598d075f6a7f2cab4f0ee9a66
Yeahyun Kim <insidesy4@gmail.com>
2024-01-29 19:25:43 +0900

    [Post] 프로그래머스 Swift 코테준비 (19) 조건 문자열 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706448902';
+const cacheName = 'chirpy-1706523907';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
