014a7384a8af54a3d70d21f74754fb90c35c79b1
Yeahyun Kim <insidesy4@gmail.com>
2024-01-30 22:52:49 +0900

    [Fix] 이미지 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706622722';
+const cacheName = 'chirpy-1706622765';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
