3f3dd9a826c0fbe6ed74277d13b2cfdff2f192ba
Yeahyun Kim <insidesy4@gmail.com>
2024-02-03 20:46:39 +0900

    [Delete] 프로그래머스 Swift 코테준비 (26) 마지막 두 원소 삭제

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1706960670';
+const cacheName = 'chirpy-1706960779';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
