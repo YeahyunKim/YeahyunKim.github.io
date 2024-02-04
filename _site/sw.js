6ec32263a94c3b99836108eb68a59ac14075bf66
Yeahyun Kim <insidesy4@gmail.com>
2024-02-04 22:22:37 +0900

    [Delete] 프로그래머스 Swift 코테준비 (22) 등차수열의 특정한 항만 더하기 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707052879';
+const cacheName = 'chirpy-1707052906';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
