7aed43a8e9f08dff3a573c868a46ea71ec1a9226
Yeahyun Kim <insidesy4@gmail.com>
2024-02-06 16:08:30 +0900

    [Post] 프로그래머스 Swift 코테준비 (28) 수 조작하기 2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707106233';
+const cacheName = 'chirpy-1707203302';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
