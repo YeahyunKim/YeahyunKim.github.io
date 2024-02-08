7043b71d12ba38bea5afa4bb9cd89a24bc447062
Yeahyun Kim <insidesy4@gmail.com>
2024-02-08 17:47:49 +0900

    [Post] 프로그래머스 Swift 코테준비 (37) 주사위 게임 3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707381057';
+const cacheName = 'chirpy-1707382039';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
