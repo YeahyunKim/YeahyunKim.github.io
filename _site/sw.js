9890de71b6231429a1882ad2f2d373937b4c5928
Yeahyun Kim <insidesy4@gmail.com>
2024-02-12 17:44:48 +0900

    [Fix] safari 이미지 깨짐 현상 수정 -> cdn으로 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707382072';
+const cacheName = 'chirpy-1707727475';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
