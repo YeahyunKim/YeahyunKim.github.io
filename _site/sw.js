e6f676ea90f63c09e2e4393670be0e2d04272959
Yeahyun Kim <insidesy4@gmail.com>
2024-02-13 18:36:36 +0900

    [Fix] 프로그래머스 Swift 코테준비 (44) 접미사 배열 코드에 변수를 추가하지 않음

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707816900';
+const cacheName = 'chirpy-1707816938';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
