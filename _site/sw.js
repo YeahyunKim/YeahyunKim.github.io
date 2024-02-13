29631b2be6e3119f4a5726908f9002ac7b4c2a91
Yeahyun Kim <insidesy4@gmail.com>
2024-02-13 17:09:57 +0900

    [Post] 프로그래머스 Swift 코테준비 (43) 문자열 뒤의 n글자

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707806888';
+const cacheName = 'chirpy-1707811761';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
