7f03a272b49d7538108adae138c014369dd1adb0
Yeahyun Kim <insidesy4@gmail.com>
2024-02-14 14:46:28 +0900

    [Post] 프로그래머스 Swift 코테준비 (46) 문자열의 앞의 n글자

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707816938';
+const cacheName = 'chirpy-1707889019';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
