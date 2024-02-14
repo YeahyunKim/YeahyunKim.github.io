5b39479e1915bd502475a16411d8af377bc67691
Yeahyun Kim <insidesy4@gmail.com>
2024-02-14 14:58:03 +0900

    [Post] 프로그래머스 Swift 코테준비 (47) 접두사인지 확인하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1707889019';
+const cacheName = 'chirpy-1707890263';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
