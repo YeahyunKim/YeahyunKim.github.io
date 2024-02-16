8d773fc4e447f916838d48cca53cf1fc8bc81752
Yeahyun Kim <insidesy4@gmail.com>
2024-02-16 19:00:26 +0900

    [Post] 프로그래머스 Swift 코테준비 (53) 글자 지우기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708072439';
+const cacheName = 'chirpy-1708077536';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
