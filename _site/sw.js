c41f46f98106e2f9a5e1119b58a931928871c9e1
Yeahyun Kim <insidesy4@gmail.com>
2024-02-17 18:25:05 +0900

    [Post] 프로그래머스 Swift 코테준비 (59) 2의 영역

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708160159';
+const cacheName = 'chirpy-1708161833';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
