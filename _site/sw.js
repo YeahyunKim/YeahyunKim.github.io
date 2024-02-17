f9c2b88f39cb97b9627945c447112c92549cf39d
Yeahyun Kim <insidesy4@gmail.com>
2024-02-17 18:51:35 +0900

    [Post] 프로그래머스 Swift 코테준비 (60) 배열 조각

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708162088';
+const cacheName = 'chirpy-1708163469';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
