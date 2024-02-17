cfa58c568bbd528eec7897bbe9080b12d1308cec
Yeahyun Kim <insidesy4@gmail.com>
2024-02-17 17:56:22 +0900

    [Post] 프로그래머스 Swift 코테준비 (58) 배열 만들기 3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708159295';
+const cacheName = 'chirpy-1708160159';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
