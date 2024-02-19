7b8016b6edaff198aeaf24df0d3199043ed1043c
Yeahyun Kim <insidesy4@gmail.com>
2024-02-19 17:00:33 +0900

    [Post] 프로그래머스 Swift 코테준비 (61) n 번째 원소부터

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708163469';
+const cacheName = 'chirpy-1708329586';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
