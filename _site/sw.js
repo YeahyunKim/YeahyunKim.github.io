1a8d14a31ad838b68a41d0a4bc52a45a8dc50390
Yeahyun Kim <insidesy4@gmail.com>
2024-02-19 18:18:46 +0900

    [Post] 프로그래머스 Swift 코테준비 (65) n개 간격의 원소

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708333250';
+const cacheName = 'chirpy-1708334275';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
