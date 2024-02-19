ec33542aaf3c7dd31de11e036c2fea53ce87a395
Yeahyun Kim <insidesy4@gmail.com>
2024-02-19 17:19:55 +0900

    [Post] 프로그래머스 Swift 코테준비 (62) 순서 바꾸기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708329586';
+const cacheName = 'chirpy-1708330773';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
