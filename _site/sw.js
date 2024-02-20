4db336d2954a52111dda15a04c7cef7d7f7a8447
Yeahyun Kim <insidesy4@gmail.com>
2024-02-20 17:52:59 +0900

    [Post] 프로그래머스 Swift 코테준비 (66) 홀수 vs 짝수

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708334275';
+const cacheName = 'chirpy-1708419156';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
