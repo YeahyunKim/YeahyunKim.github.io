a33479e9f9845c775dea8d64ed48990f95bb65f8
Yeahyun Kim <insidesy4@gmail.com>
2024-02-26 13:17:01 +0900

    [Post] 자바 빌드 도구 종류 Maven, Gradle 차이점

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708847819';
+const cacheName = 'chirpy-1708920983';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
