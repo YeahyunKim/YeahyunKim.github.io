a874aa3545a209e8cf67aca20ff2e81f5ec0ce11
Yeahyun Kim <insidesy4@gmail.com>
2024-02-27 17:13:45 +0900

    [Post] Static Content - 정적 컨텐츠

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1709005615';
+const cacheName = 'chirpy-1709021616';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
