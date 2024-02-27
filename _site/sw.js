8e93afc4839aa8468c5b48ffb5a899076a7df2e8
Yeahyun Kim <insidesy4@gmail.com>
2024-02-27 12:07:17 +0900

    [Post] 플러그인 - Spring Boot Devtools

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708920983';
+const cacheName = 'chirpy-1709003199';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
