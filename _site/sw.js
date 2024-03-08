534884cd0d0e4b05cc85ccf27e1772b431670acc
Yeahyun Kim <insidesy4@gmail.com>
2024-03-08 17:51:06 +0900

    [Post] mySQL 문법 (8) - Aliases(AS) 컬럼, 테이블명 별칭주기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1709883760';
+const cacheName = 'chirpy-1709887830';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
