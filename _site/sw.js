b767abb097e3210b80ba6e8374cdcefe3fb2e525
Yeahyun Kim <insidesy4@gmail.com>
2024-03-11 15:36:33 +0900

    [Post] JAVA study (2) - 변수 타입과 메모리 공간

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710135017';
+const cacheName = 'chirpy-1710138961';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
