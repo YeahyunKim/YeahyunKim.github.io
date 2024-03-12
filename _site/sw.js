4f61832c9096c96b306aadb20579152401104143
Yeahyun Kim <insidesy4@gmail.com>
2024-03-12 16:07:36 +0900

    [Post] JAVA study (6) - 리터럴과 상수의 차이

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710221303';
+const cacheName = 'chirpy-1710227205';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
