9aff8cfca2d1bd84df0439ea766fcb27260a8d26
Yeahyun Kim <insidesy4@gmail.com>
2024-03-13 14:31:22 +0900

    [Post] JAVA 문법 (4) - switch 조건문

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710301897';
+const cacheName = 'chirpy-1710307834';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
