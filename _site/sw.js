3bd33f156e8916f596a671e740bc080815c79330
Yeahyun Kim <insidesy4@gmail.com>
2024-03-14 16:27:49 +0900

    [Fix] 포스트 헤더 제목 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710400790';
+const cacheName = 'chirpy-1710401249';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
