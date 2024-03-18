cb78a7c160db3b3674de19825627851dd605cc04
Yeahyun Kim <insidesy4@gmail.com>
2024-03-18 12:32:17 +0900

    [Style] 블로그 전체 디자인 변경

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1710727548';
+const cacheName = 'chirpy-1710732715';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
