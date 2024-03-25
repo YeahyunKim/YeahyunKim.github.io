bb6aea37cace87019f2edf93d21ed38d2845fe82
Yeahyun Kim <insidesy4@gmail.com>
2024-03-25 13:53:11 +0900

    [Fix] 포스트 폴더링 작업

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711338405';
+const cacheName = 'chirpy-1711341919';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
