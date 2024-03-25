468d36d1e2024e4facbc98e0b75c0f9b428d3cba
Yeahyun Kim <insidesy4@gmail.com>
2024-03-25 17:59:02 +0900

    [Post] 프로그래머스-기초(11)

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711352564';
+const cacheName = 'chirpy-1711357099';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
