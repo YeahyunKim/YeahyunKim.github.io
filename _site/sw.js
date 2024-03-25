09e0c8c7a5ca10e5c19167cb7a4521745489ffa1
Yeahyun Kim <insidesy4@gmail.com>
2024-03-25 12:47:06 +0900

    [Style] blockquote 스타일 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711010383';
+const cacheName = 'chirpy-1711338405';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
