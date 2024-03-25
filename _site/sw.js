1eaf1808a6dbdcbfacd8a3c442a7bb2f3fcea645
Yeahyun Kim <insidesy4@gmail.com>
2024-03-25 18:01:18 +0900

    [Fix] md 문법 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711357099';
+const cacheName = 'chirpy-1711357206';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
