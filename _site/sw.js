ee0076630abb4594bf84bf278c713818a1bc71bf
Yeahyun Kim <insidesy4@gmail.com>
2024-03-26 15:02:58 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (16) - n의 배수

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711425239';
+const cacheName = 'chirpy-1711432955';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
