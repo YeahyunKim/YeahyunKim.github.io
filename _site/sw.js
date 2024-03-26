da4f74040f8ce17454008141c83dd250ebdcca06
Yeahyun Kim <insidesy4@gmail.com>
2024-03-26 15:30:04 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (17) - 공배수

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711432955';
+const cacheName = 'chirpy-1711434588';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
