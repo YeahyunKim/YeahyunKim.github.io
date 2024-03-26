45d8b12b0cb6f3c39192d5c62eac7b09fa4df082
Yeahyun Kim <insidesy4@gmail.com>
2024-03-26 16:29:05 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (18) - 홀짝에 따라 다른 값 반환하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711434588';
+const cacheName = 'chirpy-1711438070';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
