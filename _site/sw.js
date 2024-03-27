862e476e65cb8ad4321d747735f021787cc74e94
Yeahyun Kim <insidesy4@gmail.com>
2024-03-27 17:27:34 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (23) - 주사위 게임 2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711522582';
+const cacheName = 'chirpy-1711528036';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
