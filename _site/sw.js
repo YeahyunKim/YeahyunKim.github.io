37612c60258e3ee724235afc05610ad51deb3ae2
Yeahyun Kim <insidesy4@gmail.com>
2024-03-27 10:55:48 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (20) - flag에 따라 다른 값 반환하기

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711502053';
+const cacheName = 'chirpy-1711504329';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
