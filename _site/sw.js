628a5718ddba3aeb7dde8d69b3523e78c64dd16e
Yeahyun Kim <insidesy4@gmail.com>
2024-03-29 17:56:21 +0900

    [Add] description 추가

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711702527';
+const cacheName = 'chirpy-1711702579';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
