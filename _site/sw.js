06651dbcca45791fce17294c6a4fbd3cc5f98235
Yeahyun Kim <insidesy4@gmail.com>
2024-04-01 12:39:14 +0900

    [Post] 프로그래머스 Python 코테준비 기초 (26) - 마지막 두 원소

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1711939813';
+const cacheName = 'chirpy-1711942737';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
