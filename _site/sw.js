a9488d705867856caa22d283883972ec530ae09f
Yeahyun Kim <insidesy4@gmail.com>
2024-04-14 21:04:01 +0900

    [Fix] 포스트 수정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1712651890';
+const cacheName = 'chirpy-1713096234';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
