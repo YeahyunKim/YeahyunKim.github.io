550429b58d4e92c1a46e69ae3e00c3c5bad6d100
Yeahyun Kim <insidesy4@gmail.com>
2024-04-23 20:37:14 +0900

    [Post] Static 변수와 메모리 구조

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1713787634';
+const cacheName = 'chirpy-1713872210';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
